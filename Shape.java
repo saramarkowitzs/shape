3e279ad4769d53b0a69331ee3986085bf4d732f1
Roger Jaffe <rogerjaffe@gmail.com>
2017-07-14 09:27:40 -0700

    Initial commit

---
+++ Shape.java
@@ -0,0 +1,30 @@
+
+/**
+ * Shape class is the base class for abstracting various geometric shapes
+ * 
+ * @author Mr. Jaffe
+ * @version 2017-07-14
+ */
+public interface Shape
+{
+  /**
+   * Computes the area of the shape
+   * 
+   * @return    The computed area
+   */
+  double getArea();
+
+  /**
+   * Computes the perimeter of the shape
+   * 
+   * @return    The computed perimeter
+   */
+  double getPerimeter();
+
+  /**
+   * Gets the shape's color
+   * 
+   * @return    The color
+   */
+  double getColor();
+}
